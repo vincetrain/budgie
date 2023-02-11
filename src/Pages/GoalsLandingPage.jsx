480e407316393fc63b79fcff13e5ec440d63d47f
CreamOCorn <smileysundae3@gmail.com>
2023-02-11 06:31:59 -0500

    LandingPageGoals completed sortaa

--- src/Pages/GoalsLandingPage.jsx
+++ src/Pages/GoalsLandingPage.jsx
@@ -1,26 +1,46 @@
 import "./GoalsLandingPage.css"
 import "./ShortGoals.css"
 
-function GoalsPage(){    
+function GoalsPage() {
     return (
-    <div className = "PageContainer"> 
-        <div className = "headingsContainer">
-            <h1>Your Goals</h1>
-        </div>
-        <div className = "BoxContainer">
-            <div className = "TermContainer">
-                <h1 classname = "TermTitle"> Long Term </h1>
-                <div className = "TermBox">
+        <div className="PageContainer">
+            <div className="headingsContainer">
+                <h1>Your Goals</h1>
+            </div>
+            <div className="BoxContainer">
+                <div className="TermContainer">
+                    <h1 classname="TermTitle"> Long Term </h1>
+                    <div className="TermBox">
 
-                </div> 
-            </div> 
-            <div className = "TermContainer">
-                <h1 classname = "TermTitle"> Short Term </h1>
-                <div className = "TermBox">
-                </div> 
-            </div> 
+                        <div className="TextBox">
+                            <h3></h3>
+                            <h3></h3>
+                            <h3></h3>
+                            <h4>View</h4>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="TermContainer">
+                    <h1 classname="TermTitle"> Short Term </h1>
+                    <div className="TermBox">
+
+                        <div className="TextBox">
+                            <h3></h3>
+                            <h3></h3>
+                            <h3></h3>
+                            <h4>View</h4>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="LilQuote">
+                <p>Set your goals with us! Long AND Short!</p>
+            </div>
+
         </div>
-    </div>
     )
 }
 

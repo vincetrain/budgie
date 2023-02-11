6e3f3dfef40cdb521cf8aa3f81a96e1011fac27d
angelaaan <114760476+angelaaan@users.noreply.github.com>
2023-02-11 02:53:59 -0500

    i added a background yay! to the short/long term goals

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Home from './Pages/Home.jsx';
+import ShortGoals from './Pages/ShortGoals.jsx';
 
 import Navbar from './Components/Navbar.jsx';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,7 +16,7 @@ root.render(
 		<Routes>
 			<Route path='/' element={<Home />} />
 			<Route path='/expenses' element={<App />} />
-
+      <Route path='/ShortGoals' element={<ShortGoals />} />
 		</Routes>
 	</BrowserRouter>
 );

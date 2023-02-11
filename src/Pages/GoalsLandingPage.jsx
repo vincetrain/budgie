a9adccf0f8fa6ee626076795ce070087137ad8c1
CreamOCorn <smileysundae3@gmail.com>
2023-02-11 01:23:28 -0500

    Create Elements for GoalsLandingPage

--- src/Pages/GoalsLandingPage.jsx
+++ src/Pages/GoalsLandingPage.jsx
@@ -1,9 +1,26 @@
 import "./GoalsLandingPage.css"
+import "./ShortGoals.css"
 
 function GoalsPage(){    
     return (
-    <div className="PageContainer">
-        
+    <div className = "PageContainer"> 
+        <div className = "headingsContainer">
+            <h1>Your Goals</h1>
+        </div>
+        <div className = "BoxContainer">
+            <div className = "TermContainer">
+                <h1 classname = "TermTitle"> Long Term </h1>
+                <div className = "TermBox">
+
+                </div> 
+            </div> 
+            <div className = "TermContainer">
+                <h1 classname = "TermTitle"> Short Term </h1>
+                <div className = "TermBox">
+                    
+                </div> 
+            </div> 
+        </div>
     </div>
     )
 }

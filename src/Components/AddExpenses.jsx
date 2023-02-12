b24f90e9c891584c073c3d6adf039ed97d7a0c67
CreamOCorn <smileysundae3@gmail.com>
2023-02-11 20:19:48 -0500

    Starting ViewExpenses

--- src/Components/AddExpenses.jsx
+++ src/Components/AddExpenses.jsx
@@ -1,5 +1,5 @@
 import './AddExpenses.css';
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
 function AddExpense({state, changeState}) {
     const [newExpense, updateExpense] = useState({
